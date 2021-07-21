d2c83fc7a7e0b3563064906a32406e97b3caae9d
Eric Dodgion <Barry.manowar@gmail.com>
2021-07-20 23:35:10 -0600

    linked manifest to html

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,12 +1,12 @@
-const CACHE_NAME = "static-cache-v2";
-const DATA_CACHE_NAME = "data-cache-v1";
+const CACHE_NAME = "static-cache-v1";
+const DATA_CACHE_NAME = "data-cache";
 
 const FILES_TO_CACHE = [
   "/",
   "/index.html",
   "/indexedDb.js",
   "/index.js",
-  "/manifest.webmanifest",
+  "/manifest.json",
   "/styles.css",
   "/icons/icon-192x192.png",
   "/icons/icon-512x512.png",
@@ -14,21 +14,18 @@ const FILES_TO_CACHE = [
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches
-      .open(CACHE_NAME)
-      .then((cache) => cache.addAll(FILES_TO_CACHE))
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
   );
 
-self.skipWaiting();
+  self.skipWaiting();
 });
-
 
 // The activate handler takes care of cleaning up old caches.
 // self.addEventListener("activate", (event) => {
 //   event.waitUntil(
 //     caches.keys().then((keyList) => {
 //         return Promise.all(
-//           keyList.map((key) => { 
+//           keyList.map((key) => {
 //             if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
 //               console.log("Removing old cache data", key);
 //               return caches.delete(key);
@@ -37,31 +34,32 @@ self.skipWaiting();
 //         );
 //       })
 //     );
-    
+
 //     self.clients.claim()
 // });
 
-self.addEventListener("fetch", function(event) {
+self.addEventListener("fetch", function (event) {
   // cache successful requests to the API
   if (event.request.url.includes("/api/")) {
     event.respondWith(
       caches
-      .open(DATA_CACHE_NAME)
-      .then((cache) => {
-        return fetch(event.request)
-          .then((response) => {
-            // If the response was good, clone it and store it in the cache.
-            if (response.status === 200) {
-              cache.put(event.request.url, response.clone());
-            }
+        .open(DATA_CACHE_NAME)
+        .then((cache) => {
+          return fetch(event.request)
+            .then((response) => {
+              // If the response was good, clone it and store it in the cache.
+              if (response.status === 200) {
+                cache.put(event.request.url, response.clone());
+              }
 
-            return response;
-          })
-          .catch((err) => {
-            // Network request failed, try to get it from the cache.
-            return cache.match(event.request);
-          });
-      }).catch((err) => console.log(err))
+              return response;
+            })
+            .catch((err) => {
+              // Network request failed, try to get it from the cache.
+              return cache.match(event.request);
+            });
+        })
+        .catch((err) => console.log(err))
     );
 
     return;
@@ -70,15 +68,15 @@ self.addEventListener("fetch", function(event) {
   // if the request is not for the API, serve static assets using "offline-first" approach.
   // see https://developers.google.com/web/fundamentals/instant-and-offline/offline-cookbook#cache-falling-back-to-network
   event.respondWith(
-    fetch(event.request).catch(function (){
+    fetch(event.request).catch(function () {
       return cache.match(event.request).then((response) => {
-    //  return response || fetch(event.request);
-    if (response){
-      return response 
-    } else if (events.request.headers.get('accept').includes('text/html')){
-return catches.match('/')
-    }
-    })
+        //  return response || fetch(event.request);
+        if (response) {
+          return response;
+        } else if (events.request.headers.get("accept").includes("text/html")) {
+          return catches.match("/");
+        }
+      });
     })
   );
 });
